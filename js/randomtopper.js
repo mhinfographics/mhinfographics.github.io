546fcd6783a063748019cc265c4bcc75627441d6
Marco Hernandez <hernandezsolano@gmail.com>
2025-04-13 11:00:02 -0400

    update

--- js/randomtopper.js
+++ js/randomtopper.js
@@ -1,5 +1,5 @@
 // topper random selector total toppers +1 since starts from 0
-var toppersCount = 53;
+var toppersCount = 55;
 var randomTopper = Math.floor(Math.random() * toppersCount);
 var baseUrl = 'https://mhinfographics.github.io/img/topper/';
 document.querySelector('#topper').style.backgroundImage = `url(${baseUrl}/topper_${randomTopper}.jpg)`;
