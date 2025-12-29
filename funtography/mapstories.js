f5deb0554ad2e4d02a8f676f0d334a73cabbaa21
Marco Hernandez <hernandezsolano@gmail.com>
2025-12-29 08:22:28 -0500

    adding individual links to funtography maps

---
+++ funtography/mapstories.js
@@ -0,0 +1,48 @@
+window.onload = function () {
+    const entriesDiv = document.getElementById('entries');
+    if (entriesDiv) {
+        const latestUl = entriesDiv.querySelector('.latest');
+        const numbersUl = entriesDiv.querySelector('.numbers');
+
+        if (latestUl || numbersUl) {
+            fetch('../data/funtography.json')
+                .then(response => {
+                    if (!response.ok) {
+                        throw new Error('Network response was not ok ' + response.statusText);
+                    }
+                    return response.json();
+                })
+                .then(data => {
+                    if (latestUl) {
+                        const latestEntries = data.slice(0, 3);
+                        latestEntries.forEach(entry => {
+                            const listItem = document.createElement('li');
+                            const link = document.createElement('a');
+                            const sequence = String(entry.sequence).padStart(2, '0');
+                            link.href = `${sequence}.html`;
+                            link.textContent = entry.title;
+                            listItem.textContent = `Nº${entry.sequence} `;
+                            listItem.appendChild(link);
+                            latestUl.appendChild(listItem);
+                        });
+                    }
+
+                    if (numbersUl) {
+                        const allOtherEntries = data.slice(3);
+                        allOtherEntries.forEach(entry => {
+                            const listItem = document.createElement('li');
+                            const link = document.createElement('a');
+                            const sequence = String(entry.sequence).padStart(2, '0');
+                            link.href = `${sequence}.html`;
+                            link.textContent = `Nº${sequence}`;
+                            listItem.appendChild(link);
+                            numbersUl.appendChild(listItem);
+                        });
+                    }
+                })
+                .catch(error => {
+                    console.error('There has been a problem with your fetch operation:', error);
+                });
+        }
+    }
+};
